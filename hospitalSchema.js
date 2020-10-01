933ec8c3e7e048a94523ef0741257cbd6732532f
Namira <61814848+Namira28@users.noreply.github.com>
2020-10-01 20:48:02 +0530

    Add files via upload

---
+++ hospitalSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const HospitalSchema = new mongoose.Schema({
+
+    hId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    contactNo: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Hospital', HospitalSchema);
